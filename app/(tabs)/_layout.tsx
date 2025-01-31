4b7654c5c2d27a4683495f182ee8d80fb2e5b1c8
Zaini Rahman <zainirahman4@gmail.com>
2025-01-31 23:59:54 +0800

    Expo Tabs Clean

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#0a7ea4",
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TabOneScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Tab One</Text>
+    </View>
+  );
+}
